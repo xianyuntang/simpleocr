c0d30d4232d075613038016cd67eed45c89dfdce
xt1800i <xt1800i@gmail.com>
2020-05-01 16:29:08 +0800

    fix bugs.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('Readme.md', 'r').read()
 
 setuptools.setup(
     name="simpleocr",  #
-    version="0.0.14",
+    version="0.0.15",
     author="xt1800i",
     author_email="xt1800i@gmail.com",
     description="A ocr tool for traditional chinese",
